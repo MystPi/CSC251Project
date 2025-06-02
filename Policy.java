0451e701bf545c91bfee026fd09179f65ba6b451
MystPi <86574651+MystPi@users.noreply.github.com>
2025-06-02 09:49:41 -0400

    Update Policy.java
    Added code for Policy class.

---
+++ Policy.java
@@ -0,0 +1,127 @@
+public class Policy {
+   private int number;
+   private String providerName;
+   private String firstName;
+   private String lastName;
+   private int age;
+   private String smokingStatus;
+   private double height;
+   private double weight;
+   
+   public Policy() {
+      number = 0;
+      providerName = "";
+      firstName = "";
+      lastName = "";
+      age = 0;
+      smokingStatus = "non-smoker";
+      height = 0;
+      weight = 0;
+   }
+   
+   public Policy(
+      int num,
+      String pName,
+      String fname,
+      String lname,
+      int a,
+      String smoking,
+      double h,
+      double w
+   ) {
+      number = num;
+      providerName = pName;
+      firstName = fname;
+      lastName = lname;
+      age = a;
+      smokingStatus = smoking;
+      height = h;
+      weight = w;
+   }
+   
+   public void setNumber(int num) {
+      number = num;
+   }
+   
+   public int getNumber() {
+      return number;
+   }
+   
+   public void setProviderName(String n) {
+      providerName = n;
+   }
+   
+   public String getProviderName() {
+      return providerName;
+   }
+   
+   public void setFirstName(String n) {
+      firstName = n;
+   }
+   
+   public String getFirstName() {
+      return firstName;
+   }
+   
+   public void setLastName(String n) {
+      lastName = n;
+   }
+   
+   public String getLastName() {
+      return lastName;
+   }
+   
+   public void setAge(int a) {
+      age = a;
+   }
+   
+   public int getAge() {
+      return age;
+   }
+   
+   public void setSmokingStatus(String s) {
+      smokingStatus = s;
+   }
+   
+   public String getSmokingStatus() {
+      return smokingStatus;
+   }
+   
+   public void setHeight(double h) {
+      height = h;
+   }
+   
+   public double getHeight() {
+      return height;
+   }
+   
+   public void setWeight(double w) {
+      weight = w;
+   }
+   
+   public double getWeight() {
+      return weight;
+   }
+   
+   public double calculateBMI() {
+      return (weight * 703.0) / (height * height);
+   }
+   
+   public double calculatePolicyPrice() {
+      double price = 600;
+      
+      if (age > 50) {
+         price += 75;
+      }
+      
+      if (smokingStatus.equals("smoker")) {
+         price += 100;
+      }
+      
+      if (calculateBMI() > 35) {
+         price += (calculateBMI() - 35) * 20;
+      }
+      
+      return price;
+   }
+}
